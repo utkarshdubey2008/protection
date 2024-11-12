dc1b525103f13b643f83c03e20c66b96b94e1d9a
utkarshdubey2008 <97157730+utkarshdubey2008@users.noreply.github.com>
2024-11-12 10:10:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ API_HASH = "fb5282ed1a3ea4927718ac3415c9f644"
 BOT_TOKEN = "7900259138:AAGQbNlJOLvKtYVF1qYgOw20tsXeUR61ONA"
 OWNER_ID = 7103542064
 LOG_GROUP_ID = -1002166043196
-MONGO_URI = "mongodb+srv://editbot:editbot@cluster0.mpcmf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_URI = "mongodb+srv://editbot:editbot@cluster0.mpcmf.mongodb.net/bot_database?retryWrites=true&w=majority&appName=Cluster0"
