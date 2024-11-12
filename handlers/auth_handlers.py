02727797a70620dd8c60ef3cfa7afad0f906e395
utkarshdubey2008 <97157730+utkarshdubey2008@users.noreply.github.com>
2024-11-12 09:36:05 +0530

    Update auth_handlers.py

--- handlers/auth_handlers.py
+++ handlers/auth_handlers.py
@@ -1,10 +1,24 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
+from pymongo import MongoClient
+from config import MONGO_URI
+
+client_db = MongoClient(MONGO_URI)
+db = client_db["bot_database"]
+users_collection = db["authorized_users"]
 
 async def authorize_user(client: Client, message: Message):
     user = message.reply_to_message.from_user
-    await message.reply(f"User {user.username} has been authorized.")
+    if users_collection.find_one({"user_id": user.id}):
+        await message.reply(f"User @{user.username} is already authorized.")
+    else:
+        users_collection.insert_one({"user_id": user.id, "username": user.username})
+        await message.reply(f"User @{user.username} has been authorized.")
 
 async def unauthorize_user(client: Client, message: Message):
     user = message.reply_to_message.from_user
-    await message.reply(f"User {user.username} has been unauthorized.")
+    if users_collection.find_one({"user_id": user.id}):
+        users_collection.delete_one({"user_id": user.id})
+        await message.reply(f"User @{user.username} has been unauthorized.")
+    else:
+        await message.reply(f"User @{user.username} is not authorized.")
