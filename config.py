136bff8be94333f9034bd48fa09a5adfce438a40
utkarshdubey2008 <97157730+utkarshdubey2008@users.noreply.github.com>
2024-11-12 08:56:01 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+API_ID = "your_api_id"
+API_HASH = "your_api_hash"
+BOT_TOKEN = "your_bot_token"
+OWNER_ID = your_owner_id
+LOG_GROUP_ID = your_log_group_id
+MONGO_URI = "mongodb://username:password@host:port/database"
